a51a6d8c1561ec256a57fab235fa729c70036221
dereking <kedereking@gmail.com>
2026-08-14 00:11:01 +0800

    test: validate ESP32 partition flags

--- tests/esp32_layout.sh
+++ tests/esp32_layout.sh
@@ -8,6 +8,14 @@ partition_file="$firmware_dir/partitions_8m.csv"
 sdkconfig_file="$firmware_dir/sdkconfig.defaults"
 component_file="$firmware_dir/main/idf_component.yml"
 
+if [[ -n "${MICRO_ESP32_LAYOUT_PARTITIONS:-}" ]]; then
+  [[ "${MICRO_ESP32_LAYOUT_TEST_MODE:-}" == 1 ]] || {
+    print -u2 -- "esp32_layout: partition override requires MICRO_ESP32_LAYOUT_TEST_MODE=1"
+    exit 1
+  }
+  partition_file="$MICRO_ESP32_LAYOUT_PARTITIONS"
+fi
+
 fail() {
   print -u2 -- "esp32_layout: $1"
   exit 1
@@ -18,12 +26,12 @@ for required_file in "$partition_file" "$sdkconfig_file" "$component_file"; do
 done
 
 typeset -a expected_rows=(
-  'nvs|data|nvs|0x9000|0x6000'
-  'phy_init|data|phy|0xF000|0x1000'
-  'factory|app|factory|0x10000|0x380000'
-  'micro_cfg|data|nvs|0x390000|0x10000'
-  'micro_apps|data|littlefs|0x3A0000|0x440000'
-  'coredump|data|coredump|0x7E0000|0x20000'
+  'nvs|data|nvs|0x9000|0x6000|'
+  'phy_init|data|phy|0xF000|0x1000|'
+  'factory|app|factory|0x10000|0x380000|'
+  'micro_cfg|data|nvs|0x390000|0x10000|'
+  'micro_apps|data|littlefs|0x3A0000|0x440000|'
+  'coredump|data|coredump|0x7E0000|0x20000|'
 )
 
 typeset -a actual_rows
@@ -33,14 +41,14 @@ done < <(
   awk -F, '
     /^[[:space:]]*#/ || /^[[:space:]]*$/ { next }
     {
+      if (NF != 6) {
+        printf "invalid partition row %d: expected 6 columns, found %d\n", NR, NF > "/dev/stderr"
+        exit 2
+      }
       for (i = 1; i <= 6; i++) {
         gsub(/^[[:space:]]+|[[:space:]]+$/, "", $i)
       }
-      if (NF < 5) {
-        printf "invalid partition row %d\n", NR > "/dev/stderr"
-        exit 2
-      }
-      printf "%s|%s|%s|%s|%s\n", $1, $2, $3, $4, $5
+      printf "%s|%s|%s|%s|%s|%s\n", $1, $2, $3, $4, $5, $6
     }
   ' "$partition_file"
 )
@@ -53,7 +61,7 @@ for index in {1..${#expected_rows[@]}}; do
   [[ "${actual_rows[$index]}" == "${expected_rows[$index]}" ]] || \
     fail "partition $index differs: expected '${expected_rows[$index]}', found '${actual_rows[$index]}'"
 
-  IFS='|' read -r name type subtype offset size <<< "${actual_rows[$index]}"
+  IFS='|' read -r name type subtype offset size flags <<< "${actual_rows[$index]}"
   typeset offset_value=$(( offset ))
   typeset size_value=$(( size ))
   (( size_value > 0 )) || fail "$name has a non-positive size"
@@ -96,5 +104,33 @@ typeset -a component_contract=(
 for dependency in "${component_contract[@]}"; do
   rg -Fqx -- "  $dependency" "$component_file" || fail "missing pinned dependency: $dependency"
 done
+
+if [[ "${MICRO_ESP32_LAYOUT_TEST_MODE:-}" != 1 ]]; then
+  mutation_dir=$(/usr/bin/mktemp -d "${TMPDIR:-/tmp}/micro-layout-test.XXXXXX")
+  trap '/bin/rm -rf -- "$mutation_dir"' EXIT
+  typeset -i mutation_failures=0
+
+  invalid_flag="$mutation_dir/invalid-flag.csv"
+  awk -F, 'BEGIN { OFS="," } NR == 2 { $6="encrypted" } { print }' \
+    "$partition_file" > "$invalid_flag"
+  if MICRO_ESP32_LAYOUT_TEST_MODE=1 \
+    MICRO_ESP32_LAYOUT_PARTITIONS="$invalid_flag" \
+    zsh "$0" >/dev/null 2>&1; then
+    print -u2 -- "esp32_layout: partition parser accepted a non-empty flag"
+    (( mutation_failures += 1 ))
+  fi
+
+  extra_column="$mutation_dir/extra-column.csv"
+  awk 'NR == 2 { $0=$0 ",unexpected" } { print }' \
+    "$partition_file" > "$extra_column"
+  if MICRO_ESP32_LAYOUT_TEST_MODE=1 \
+    MICRO_ESP32_LAYOUT_PARTITIONS="$extra_column" \
+    zsh "$0" >/dev/null 2>&1; then
+    print -u2 -- "esp32_layout: partition parser accepted a seventh column"
+    (( mutation_failures += 1 ))
+  fi
+
+  (( mutation_failures == 0 )) || fail "$mutation_failures negative mutation cases failed"
+fi
 
 print -- "ESP32 8 MB firmware layout contract passed"
